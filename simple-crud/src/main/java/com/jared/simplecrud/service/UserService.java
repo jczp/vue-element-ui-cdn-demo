b9ea9e62ea1412d53c5224c7b55d5ef3b71d7218
Jared <85867271@qq.com>
2022-06-06 20:46:30 +0800

    'commit'

--- simple-crud/src/main/java/com/jared/simplecrud/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.jared.simplecrud.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.jared.simplecrud.entity.User;
-
-public interface UserService extends IService<User> {
-}
